849422bd89b3bbff01a35cad2275fccaee6fe882
manlikleo <olaotan104@gmail.com>
2021-04-08 09:33:02 +0300

    created using both es5 and es6 oop syntax

--- app.js
+++ app.js
@@ -1,4 +1,123 @@
+// ES5 classes
+
+// Book Constructor
+function Book(title,author,recomm){
+    this.title = title;
+    this.author = author;
+    this.recomm = recomm;
+};
+
+
+const bookForm = document.getElementById('book-form'),
+    booktitle = bookForm.booktitle,
+    recom = bookForm.recom,
+    author = bookForm.author;
+
+
+const List = document.getElementById('booklist');
+
+
+
+//functionality Constructor
+function App(){};
+
+
+// add book to list
+App.prototype.addBookToList = function(data){
+    // wrapper 
+ 
+    // creating table row
+    const row = document.createElement('tr');
+
+  row.innerHTML = `
+  <td>${data.title}</td>
+  <td>${data.recomm}</td>
+  <td>${data.author}</td>
+  <td  ><i  class="fas fa-times delete"></i></td>
+  `;
+  List.appendChild(row);
+  
+}
+
+
+// clear form
+App.prototype.clearForm = function (){
+    bookForm.reset();
+}
+
+
+App.prototype.ShowAlert = function(messg,Eleclass){
+
+    const errorcontainer = document.createElement('div');
+    
+    const appContainer = document.querySelector('.container');
+
+    errorcontainer.classList.add(Eleclass);
+    errorcontainer.appendChild(document.createTextNode(messg));
+
+    errorcontainer.style.padding = '2% 4%';
+    
+   appContainer.insertBefore(errorcontainer,bookForm);
+  
+   setTimeout(function(){
+       errorcontainer.remove();
+   },3000)
+ 
+}
+
+
+
+App.prototype.deletedItem = function (targetE){
+ 
+    if(targetE){
+        let row = targetE.parentElement.parentElement;
+        row.remove();
+        console.log(row);
+    }else{
+        console.log("you're not on target");
+    }
+
+}
+
 
 document.addEventListener('DOMContentLoaded', function(){
-    console.log('content reloaded');
-})
+
+    bookForm.addEventListener('submit', e=>{
+
+        e.preventDefault();
+        const appfunc = new App()
+
+        if(booktitle.value === '' || recom.value === '' || author.value === ''){
+             console.log('one of your input is empty');
+             appfunc.ShowAlert('Kindly All Inputs','error');
+        
+        }else{
+
+            const book = new Book(booktitle.value,author.value,recom.value);
+           
+            appfunc.addBookToList(book)
+            appfunc.clearForm();
+            appfunc.ShowAlert('New Book added to your library', 'success');
+        }
+        
+        
+    });
+
+
+    List.addEventListener('click', e => {
+
+        e.preventDefault();
+        let tRow = e.target.closest('i');
+
+        let appfunc = new App();
+
+        appfunc.deletedItem(tRow);
+    
+    
+    })
+
+   
+
+});
+
+
---
+++ app_2.js
@@ -0,0 +1,183 @@
+// converting it all classes to ES6
+
+class Book {
+    constructor(title,author,recomm,id){
+        this.title = title;
+        this.author = author;
+        this.recomm = recomm;
+        this.id = id;
+    }
+}
+
+const bookForm = document.getElementById('book-form'),
+    booktitle = bookForm.booktitle,
+    recom = bookForm.recom,
+    author = bookForm.author;
+    let id = new Date().getTime();
+   
+
+const List = document.getElementById('booklist');
+
+class App {
+
+    addBooktoList(data){
+
+    // creating table row
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${data.title}</td>
+    <td>${data.recomm}</td>
+    <td>${data.author}</td>
+    <td id="${data.id}"><i  class="fas fa-times delete"></i></td>
+    `;
+
+    List.appendChild(row);
+
+    }
+
+
+
+    clearForm(){
+        bookForm.reset();
+    }
+
+    showAlert(messg,Eleclass){
+
+        const errorcontainer = document.createElement('div');
+        
+        const appContainer = document.querySelector('.container');
+
+        errorcontainer.classList.add(Eleclass);
+        errorcontainer.appendChild(document.createTextNode(messg));
+
+        errorcontainer.style.padding = '2% 4%';
+        
+        appContainer.insertBefore(errorcontainer,bookForm);
+  
+        setTimeout(function(){
+        errorcontainer.remove();
+        },3000)
+
+    }
+
+    deleteItem(targetE){
+
+        if(targetE){
+            let row = targetE.parentElement.parentElement;
+            row.remove();
+            console.log(row);
+
+        }else{
+            console.log("you're not on target");
+        }
+    }
+
+}
+
+class store {
+
+
+   
+    
+    static getBooks(){
+        let books;
+        
+        if (localStorage.getItem('books') === null){
+            books = [];
+        }else{
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+    }
+
+    static  displayBooks(){
+
+        const books = store.getBooks();
+
+        books.forEach(book => {
+            const Appfun = new App();
+            Appfun.addBooktoList(book);
+        });
+
+    }
+
+    static  addBooks(data){
+    
+        const books = store.getBooks();
+
+        books.push(data);
+        localStorage.setItem('books',JSON.stringify(books)); 
+          
+    }
+
+    static  removeBooks(row){
+        const books = store.getBooks();
+        
+        books.forEach((book,index) => {
+            if(book.id === parseInt(row)){
+                books.splice(index,1);
+            }
+        });
+      
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    store.displayBooks();
+
+    bookForm.addEventListener('submit', e=>{
+
+        
+
+        e.preventDefault();
+        const appfunc = new App()
+        // const locStore = new store() we don't need to externciate it because it's a static method 
+
+        if(booktitle.value === '' || recom.value === '' || author.value === ''){
+             console.log('one of your input is empty');
+             appfunc.showAlert('Kindly All Inputs','error');
+        
+        }else{
+            
+            const book = new Book(booktitle.value,author.value,recom.value,id);
+
+            appfunc.addBooktoList(book)
+
+            // add books to local storage 
+            store.addBooks(book);
+            
+            appfunc.clearForm();
+            appfunc.showAlert('New Book added to your library', 'success');
+        }
+        
+        
+    });
+
+
+    List.addEventListener('click', e => {
+
+        e.preventDefault();
+
+        let appfunc = new App();
+        let tRow = e.target.closest('i');
+
+        let row = tRow.parentElement.id;
+        console.log(row);
+        appfunc.deleteItem(tRow);
+
+        store.removeBooks(row);
+
+
+        appfunc.showAlert('Book successfully removed ', 'error');
+    
+    
+    })
+
+});  
+
