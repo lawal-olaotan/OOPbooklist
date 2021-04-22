f01ab34999fc42408b3b905d8b3aae3b4b9907f0
manlikleo <olaotan104@gmail.com>
2021-04-23 00:31:39 +0300

    homepage functionalities

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,54 @@
-const slidercont = document.querySelector('#container');
-const nextBtn = document.querySelector('#nextbtn');
-const glider = slidercont.querySelector('.glider')
+const nextBtn = document.querySelector('#nextbtn'),
+ mobileView = window.matchMedia("(max-width:450px)"),
+ reviewcarol = document.querySelector('#review-carousel'),
+ carol = document.querySelector('.main-carousel');
+
+ let flicky;
+
+
+ function flick(flickEle,dots,prevandNext,autoP,gCell){
+
+    flicky = new Flickity(flickEle, {
+        cellAlign:'left',
+        contain:false,
+        freeScroll:true,
+        pageDots:dots,
+        prevNextButtons:prevandNext,
+        autoPlay:autoP,
+        groupCells:gCell,
+        pauseAutoPlayOnHover: false,
+
+    });
+
+}
+
+ 
+
+
 
 document.addEventListener('DOMContentLoaded', function(){
 
-    new Glider(glider),{
-        slidesToShow:3,
-        slidesToScroll:2,
-        draggble:true,
-        arrows:{
-            next: nextBtn,
+    // intilizing flick for review carousel and books card 
+    flick(carol,false,true,false,true);
+    flick(reviewcarol,true,false,true,false);
+
+    // the view all button
+    nextBtn.addEventListener('click', e =>{
+        e.preventDefault();
+
+        if(mobileView.matches){
+            index = 5; 
+        }else{
+           index = 3;
         }
-    }
-    console.log('content loaded');
+        flick(carol,false,true);
+
+        flicky.next(false,false);
+
+    });
+
+
+
     
 });
-
 
