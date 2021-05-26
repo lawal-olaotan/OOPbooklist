e64a3b17f421c08e9fb7c16500232f5452eef621
manlikleo <olaotan104@gmail.com>
2021-05-26 13:52:36 +0300

    finalize

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-
-
-dbPassword = 'mongodb+srv://Leo:'+ encodeURIComponent('Queens1995') +'@book-keeper.lfcrr.mongodb.net/Book-Keeper?retryWrites=true&w=majority';
-module.exports = {
-    mongoURI: dbPassword
-};
