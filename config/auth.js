799707114dcc5a9570fa3f2983d4c8973fb435a5
manlikleo <olaotan104@gmail.com>
2021-05-26 13:53:25 +0300

    ignore files

---
+++ config/auth.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    ensureAuthenicated:function(req,res,next){
+        if(req.isAuthenticated()){
+            return next();
+        }
+        req.flash('error_msg','please login to add books');
+        res.redirect('/users/login');
+    },
+
+    forwardAuthenticated: function(req, res, next) {
+      if (!req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect('/dashboard/books');      
+    }
+
+}
